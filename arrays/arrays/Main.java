a268f471e98c3bd57b937f64783ed30a752f1f9c
Rohith_Ramanadham <rohith.ramanadham@dell.com>
2025-12-31 21:06:44 +0530

    A54: SpiralOrder

--- arrays/arrays/Main.java
+++ arrays/arrays/Main.java
@@ -7,16 +7,20 @@ import arrays.easy.TwoSum1;
 import arrays.medium.NextPerm31;
 import arrays.medium.RearrangeArray2149;
 import arrays.medium.RotateArray189;
+import arrays.medium.Spiral54;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
-        NextPerm31 n = new NextPerm31();
-        System.out.println(Arrays.toString(n.nextPermutation(arr)));
+        // NextPerm31 n = new NextPerm31();
+        // System.out.println(Arrays.toString(n.nextPermutation(arr)));
+
+        Spiral54 s = new Spiral54();
+        System.out.println(s.spiralOrder(arr));
         
         //test-pr
     }
